b44d75cd1f44ef19646e9e7f64a8d36e2588aff5
raycewiggins <raycewiggins@gmail.com>
2016-05-26 21:55:09 -0500

    Assignment 20 completed

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -13,7 +13,8 @@ require_relative '../models/address_book'
      puts "2 - Create an entry"
      puts "3 - Search for an entry"
      puts "4 - Import entries from a CSV"
-     puts "5 - Exit"
+     puts "5 - View entry number n"
+     puts "6 - Exit"
      print "Enter your selection: "
 
      selection = gets.to_i
@@ -35,14 +36,18 @@ require_relative '../models/address_book'
          read_csv
          main_menu
        when 5
+         system "clear"
+         view_entry_number
+         main_menu
+       when 6
          puts "Good-bye!"
          exit(0)
        else
          system "clear"
          puts "Sorry, that is not a valid input"
          main_menu
+       end
       end
-    end
 
       def view_all_entries
         address_book.entries.each do |entry|
@@ -50,25 +55,22 @@ require_relative '../models/address_book'
           puts entry.to_s
           entry_submenu(entry)
         end
-
         system "clear"
         puts "End of entries"
       end
 
       def create_entry
-       system "clear"
-       puts "New AddressBloc Entry"
-       print "Name: "
-       name = gets.chomp
-       print "Phone number: "
-       phone = gets.chomp
-       print "Email: "
-       email = gets.chomp
-
-       address_book.add_entry(name, phone, email)
-
-       system "clear"
-       puts "New entry created"
+         system "clear"
+         puts "New AddressBloc Entry"
+         print "Name: "
+         name = gets.chomp
+         print "Phone number: "
+         phone = gets.chomp
+         print "Email: "
+         email = gets.chomp
+         address_book.add_entry(name, phone, email)
+         system "clear"
+         puts "New entry created"
       end
 
       def search_entries
@@ -82,7 +84,6 @@ require_relative '../models/address_book'
         puts "d - delete entry"
         puts "e - edit this entry"
         puts "m - return to main menu"
-
         selection = gets.chomp
         case selection
           when "n"
@@ -96,6 +97,20 @@ require_relative '../models/address_book'
             puts "#{selection} is not a valid input"
             entry_submenu(entry)
          end
+      end
+
+      def view_entry_number
+        print "Enter the number you wish to search: "
+        query = gets.chomp.to_i
+          if query < address_book.entries.count
+            puts address_book.entries[query]
+            puts "Press Enter to Exit"
+            gets.chomp
+            system "clear"
+          else
+            puts "Selection is invalid"
+            view_entry_number
+          end
       end
 
    end
